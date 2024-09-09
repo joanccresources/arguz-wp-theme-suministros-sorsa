274a850b881a3bec61b09ad17b78d642f42d4187
Joan C <joan0492@gmail.com>
2024-09-09 18:46:44 -0500

    Se cambio enlace a donde direcciona el boton nosotros

--- assets/js/nosotros.js
+++ assets/js/nosotros.js
@@ -10,12 +10,12 @@ const addButtonInValores = () => {
 >
   <div class="elementor-widget-container">
     <a
-      href="https://api.whatsapp.com/send?phone=51954691632&amp;text=%C2%A1Hola,%20deseo%20precios%20al%20por%20mayor%20de%20sorsa!"
+      href="https://corporativa.sorsa.pe/nosotros/"
       target="_blank"
       class="artech-button fade-border-effect"
     >
       <span class="artech-button-content-wrapper">
-        <span class="artech-button-text"> Contactar a un asesor </span>
+        <span class="artech-button-text">Conoce nuestra cultura corporativa</span>
         <img
           decoding="async"
           src="https://suministros.sorsa.pe/wp-content/uploads/2024/03/arrow_wh.svg"
